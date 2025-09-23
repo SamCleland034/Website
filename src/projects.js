fd083e8dc9bebbde11f492c9e95e5a43389bba8f
Sam Cleland <edward.cleland@mail.mcgill.ca>
2025-09-23 11:39:05 -0400

    Fixed grammar and added more detail to projects

--- src/projects.js
+++ src/projects.js
@@ -7,12 +7,12 @@ function Projects() {
   const projects = [
     {
       title: "NCAA Basketball Predictor",
-      description: "A neural network to predict 2025 NCAA Tournament game outcomes using Python.",
+      description: "•	Competition where teams were tasked with creating a model with any data to predict probabilities of March Madness games. Finished 2nd place out of 10 teams by creating a TensorFlow SNN model using provided data as well as data from external sources such as CBB on Kaggle, altitude data of campuses and basketball-reference.",
       link: "https://github.com/SamCleland034/March-Madness",
     },
     {
       title: "Real Time Trading Algorithm",
-      description: "Algorithmic Trading Dashboard showing the trades the script is running, base amount starts at 100k (paper trading) ",
+      description: "Algorithmic Trading Dashboard showing the trades the script is running, base amount starts at 100k (paper trading). ",
       link:"https://github.com/SamCleland034/Trading-Algorithm",
     },
     {
