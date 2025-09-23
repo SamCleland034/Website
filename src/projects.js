06ee087b471909bd79f9cc8b60c988a23db12fb9
Sam Cleland <edward.cleland@mail.mcgill.ca>
2025-09-23 12:30:26 -0400

    Added more projects

--- src/projects.js
+++ src/projects.js
@@ -7,7 +7,7 @@ function Projects() {
   const projects = [
     {
       title: "NCAA Basketball Predictor",
-      description: "•	Competition where teams were tasked with creating a model with any data to predict probabilities of March Madness games. Finished 2nd place out of 10 teams by creating a TensorFlow SNN model using provided data as well as data from external sources such as CBB on Kaggle, altitude data of campuses and basketball-reference.",
+      description: "Competition where teams were tasked with creating a model with any data to predict probabilities of March Madness games using a TensorFlow SNN model. Finished 2nd place out of 10 teams.",
       link: "https://github.com/SamCleland034/March-Madness",
     },
     {
@@ -20,7 +20,16 @@ function Projects() {
       description: "•	Developed automated NLP pipeline to extract, preprocess, and classify contract clauses into low/moderate/high risk categories using regex for extraction, NLTK/WordNet for lemmatization/tokenization, and Bidirectional LSTM model in TensorFlow.",
       link:"https://github.com/SamCleland034/NLP-Contract-Risk-Analyzer",
     },
-
+    {
+      title: "Robot Obstacle Course",
+      description: "Created Robot using Java Lejos EV3 to play a game of Capture the Flag on an obstacle course. Robot had to traverse a zipline in order to complete the course. Finished first place out of 20 teams.",
+      link: "https://github.com/SamCleland034/ECSE-211-Design-Team-3"
+    },
+    {
+      title: "Modified Monte Carlo Tree Search AI for Board Game",
+      description: "Implemented variant of MCTS with modified Upper Confidence Bound to simulate both player and opponent actions in a game called Pentago-Swap, incorporating minimax-like minimization for opponent turns to better model adversarial play. Finished in top 5% of students.",
+      link: "https://github.com/SamCleland034/COMP-424"
+    }
 
   ];
 
